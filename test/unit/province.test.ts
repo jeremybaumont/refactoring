94b744b9534c1f6dd3934e9ae6d148cd32db8cf9
Jeremy Baumont <jeremy.baumont@gmail.com>
2021-12-03 11:30:54 +1100

    refactor: move variable asiaProvince to test setup

--- test/unit/province.test.ts
+++ test/unit/province.test.ts
@@ -1,14 +1,24 @@
 import { producerBuilder } from '../support/builders/producer-builder';
 import { provinceBuilder } from '../support/builders/province-builder';
+import { Province } from '../../src/province';
 
 describe('province', function() {
-    it('shortfall', function() {
-        const asiaProvince = provinceBuilder.withName('Asia').withDemand(30).withPrice(20).withProducers(
+
+    let asiaProvince: Province;
+    beforeEach(() => {
+        asiaProvince = provinceBuilder.withName('Asia').withDemand(30).withPrice(20).withProducers(
             producerBuilder.withName('Byzantium').withCost(10).withProduction(9),
             producerBuilder.withName('Attalia').withCost(12).withProduction(10),
             producerBuilder.withName('Synope').withCost(10).withProduction(6),
         ).build();
+    });
 
+    it('shortfall', function() {
         expect(asiaProvince.shortfall).toBe(5);
     });
+
+    it('profit', function() {
+        expect(asiaProvince.profit).toBe(230);
+    });
+
 });
