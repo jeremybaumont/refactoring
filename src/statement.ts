167827c13a6b9ab5b1962f030b5707501168f65d
Jeremy Baumont <jeremy.baumont@gmail.com>
2021-12-04 17:45:16 +1100

    refactor: move division into usd function

--- src/statement.ts
+++ src/statement.ts
@@ -58,7 +58,7 @@ export const statement = (invoice: InvoiceData, plays: PlaysData): string => {
             {
                 style: "currency", currency: "USD",
                 minimumFractionDigits: 2
-            }).format(aNumber);
+            }).format(aNumber / 100);
     }
 
 
@@ -67,10 +67,10 @@ export const statement = (invoice: InvoiceData, plays: PlaysData): string => {
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+    result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 }
