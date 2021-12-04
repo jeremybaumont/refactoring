d121a56df74d2fb17648dc3e7608c91e9796c862
Jeremy Baumont <jeremy.baumont@gmail.com>
2021-12-04 15:43:58 +1100

    refactor: extract function amountFor

--- src/statement.ts
+++ src/statement.ts
@@ -29,24 +29,7 @@ export const statement = (invoice: InvoiceData, plays: PlaysData): string => {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID] ?? { type: "not-type-found", name: "no-name-found" };
-        let thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(play, perf);
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
@@ -57,5 +40,27 @@ export const statement = (invoice: InvoiceData, plays: PlaysData): string => {
     }
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
+    return result;
+}
+
+const amountFor = (play: PlayData, perf: Perf): number => {
+    let result = 0;
+    switch (play.type) {
+        case "tragedy":
+            result = 40000;
+            if (perf.audience > 30) {
+                result += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            result = 30000;
+            if (perf.audience > 20) {
+                result += 10000 + 500 * (perf.audience - 20);
+            }
+            result += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
     return result;
 }
