be55ff0dfe150c16c2280d4256acd6cbee8e7d89
Jeremy Baumont <jeremy.baumont@gmail.com>
2021-12-04 18:11:57 +1100

    refactor: extract function totalAmount

--- src/statement.ts
+++ src/statement.ts
@@ -69,12 +69,19 @@ export const statement = (invoice: InvoiceData, plays: PlaysData): string => {
         return volumeCredits;
     }
 
-    let totalAmount = 0;
+    const foobar = (): number => {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
+    }
+
     let result = `Statement for ${invoice.customer}\n`;
     for (let perf of invoice.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
     }
+    let totalAmount = foobar();
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${totalVolumeCredits()} credits\n`;
     return result;
